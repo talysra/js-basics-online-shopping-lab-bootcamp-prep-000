fc9314dd8ccca80400a25555f88581e84e228c68
Stanislava Koklina <medula_oblomgata@mail.ru>
2017-10-05 20:56:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,21 +18,32 @@ function addToCart(item) {
   return cart;
 }
 
-function viewCart() {
+var cartStatement = [];
   if (cart.length === 0) {
-    console.log("Your shopping cart is empty.");
+    console.log('Your shopping cart is empty.');
+   }
+   else {
+     for (let i = 0; i < cart.length; i++) {
+       var myItems = Object.keys(cart[i]);
+       var itemsPrices = cart[i][myItems];
+       var itemsAndPrices = ' ' + myItems + ' at $' + itemsPrices;
+       cartStatement.push(itemsAndPrices);
+     }
+     if (cartStatement.length === 1) {
+       console.log('In your cart, you have' + cartStatement + '.');
+     }
+     else if (cartStatement.length == 2) {
+       itemsAndPrices = cartStatement.join(' and');
+       console.log(`In your cart, you have${itemsAndPrices}.`);
+     }
+     else {
+       cartStatement[cartStatement.length - 1] = ' and'.concat(
+         cartStatement[cartStatement.length - 1]);
+       var cartStatementUpdated = cartStatement.join(',');
+       console.log('In your cart, you have' + cartStatementUpdated + '.');
+     }
   }
-else {
-  for (var i = 0; i < cart.length; i++) {
-    var myItems = Object.keys(cart[i]);
-    var itemsPrices = cart[i][myItems];
-    console.log(` ${myItems} at $ ${itemsPrices}`)
   }
-}
-if (true) {
-
-}
-}
 
 function total() {
   // write your code here
