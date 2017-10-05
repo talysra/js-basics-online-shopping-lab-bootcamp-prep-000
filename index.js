a618efd112753b06df7380a0c885e6ead3a1f7bf
Stanislava Koklina <medula_oblomgata@mail.ru>
2017-10-05 20:58:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,5 +73,12 @@ function removeFromCart(item) {
            }}
 
 function placeOrder(cardNumber) {
-  // write your code here
+  function placeOrder(cardNumber){
+       if (cardNumber) {
+         console.log('Your total cost is $' + total() +', which will be charged to the card ' + cardNumber+'.');
+         cart = []
+       }
+       else {
+         console.log('Sorry, we don\'t have a credit card on file for you.');
+     }
 }
